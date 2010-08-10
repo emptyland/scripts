ef1f90155cb76846e577a0851eae695d02fb8ff9
Niko Bellic <cjvoid@gmail.com>
2010-08-10 23:22:19 +0000

    btdown OOP added.

--- btdown/torrents.py
+++ btdown/torrents.py
@@ -8,13 +8,78 @@ import hashlib, urllib, re, sys, sqlite3, os, time
 rt_log = None
 
 class btdown_log:
-	fd = None
 	def __init__(self, record_path):
 		self.fd = open(record_path, 'a')
-	def log(self, msg, level = 0):
-		line = u'[%d] [%s] %s\n' % (level, time.ctime(), msg)
+		
+	def log(self, msg):
+		line = '[%s] %s\n' % (time.ctime(), msg)
 		self.fd.write(line)
 		self.fd.flush()
+
+
+class torrent_history:
+	table_sql = 'CREATE TABLE history (url TEXT PRIMARY KEY NOT NULL, hash TEXT NOT NULL)'
+	url_sql = 'SELECT COUNT(*) FROM history WHERE url=\'%s\''
+	torrent_sql = 'SELECT COUNT(*) FROM history WHERE hash=\'%s\''
+	record_sql = 'INSERT INTO history VALUES (\'%s\', \'%s\')'
+        
+	def __init__(self, db_path):
+		"""
+		Initizlizate database file by sqlite3
+		If database is not exist, create a new one.
+		"""
+		self.conn = sqlite3.connect(db_path)
+		sql = self.conn.cursor()
+		try:
+			sql.execute(self.table_sql)
+		except sqlite3.OperationalError:
+			pass
+					
+	def url_never_downloaded(self, url):
+		"""
+		Check did URL download first.
+		"""
+		sql = self.conn.cursor()
+		sql.execute(self.url_sql % (url))
+		for row in sql:
+			return not row[0]
+
+	def torrent_never_downloaded(self, filename):
+		"""
+		We need check did torrent file download.
+		"""                
+		digest = hashlib.sha1(filename).hexdigest()
+		sql = self.conn.cursor()
+		sql.execute(self.torrent_sql % (digest))
+		for row in sql:
+			return not row[0]
+
+	def record(self, filename, url):
+		"""
+		Record URL string and torrent SHA1 digest.
+		"""
+		digest = hashlib.sha1(filename).hexdigest()
+		sql = self.conn.cursor()
+		sql.execute(self.record_sql % (url, digest))
+		self.conn.commit()
+
+
+class btdown_conf:
+	def __init__(self, conf_path):
+		"""
+		Read rss urls and reguler keywords from xml configure file.
+		"""
+		xmldoc = minidom.parse(conf_path)
+		self.cmd = xmldoc.getElementsByTagName('cmd')[0].firstChild.data
+		self.log_path = xmldoc.getElementsByTagName('log')[0].firstChild.data
+		self.pid_path = xmldoc.getElementsByTagName('pid')[0].firstChild.data
+		self.db_path = xmldoc.getElementsByTagName('db')[0].firstChild.data
+		# Get RSS URLs
+		items = xmldoc.getElementsByTagName('rss')[0].getElementsByTagName('entry')
+		self.rss_urls = [ item.firstChild.data for item in items ]
+		# Get regular keywords
+		items = xmldoc.getElementsByTagName('keys')[0].getElementsByTagName('entry')
+		self.regular_keywords = [ item.firstChild.data for item in items ]
 
 
 def match_keys(repo, keys):
@@ -40,35 +105,9 @@ def get_target_torrent_urls(xmldoc, keys):
 		title = item.getElementsByTagName('title')[0].firstChild.data
 		if match_keys(title, keys):
 			attr = item.getElementsByTagName('enclosure')[0].attributes['url']
-			#rt_log.log('%s matched, URL : %s' % (title, attr.value))
+			rt_log.log('\"%s\" matched, URL : %s' % (title, attr.value))
 			urls.append(attr.value)
 	return urls
-
-def init_database(conn_string):
-	"""
-	Initizlizate database file by sqlite3
-	If database is not exist, create a new one
-	"""
-	conn = sqlite3.connect(conn_string)
-	sql = conn.cursor()
-	try:
-		sql.execute('CREATE TABLE history (url TEXT PRIMARY KEY NOT NULL, hash TEXT NOT NULL)')
-	except sqlite3.OperationalError:
-		return conn
-	return conn
-
-def init_config(filename):
-	"""
-	Read rss urls and reguler keywords from xml configure file.
-	"""
-	xmldoc = minidom.parse(filename)
-	cmd = xmldoc.getElementsByTagName('cmd')[0].firstChild.data
-	record_path = xmldoc.getElementsByTagName('log')[0].firstChild.data
-	items = xmldoc.getElementsByTagName('rss')[0].getElementsByTagName('entry')
-	rss_urls = [ item.firstChild.data for item in items ]
-	items = xmldoc.getElementsByTagName('keys')[0].getElementsByTagName('entry')
-	regular_keywords = [ item.firstChild.data for item in items ]
-	return (cmd, record_path, rss_urls, regular_keywords)
 
 def fetch_torrent(filename, url):
 	"""
@@ -79,68 +118,63 @@ def fetch_torrent(filename, url):
 	fd.write(conn.read())
 	fd.close()
 
-def url_never_downloaded(url, conn):
-	sql = conn.cursor()
-	sql.execute('SELECT COUNT(*) FROM history WHERE url=\'%s\'' % (url))
-	for row in sql:
-		return not row[0]
-
-def torrent_never_downloaded(filename, conn):
-	digest = hashlib.sha1(filename).hexdigest()
-	sql = conn.cursor()
-	sql.execute('SELECT COUNT(*) FROM history WHERE hash=\'%s\'' % (digest))
-	for row in sql:
-		return not row[0]
-
-def log_torrent(filename, url, conn):
-	digest = hashlib.sha1(filename).hexdigest()
-	sql = conn.cursor()
-	sql.execute('INSERT INTO history VALUES (\'%s\', \'%s\')' % (url, digest))
-	conn.commit()
-
-def bt_download_target(cmd, url, conn):
+def bt_download_target(cmd, url, history):
 	"""
 	BT download a target torrent.
 	"""
 	global rt_log
 	generator = hashlib.md5(os.urandom(16))
-	if url_never_downloaded( url, conn ):
+	rt_log.log('BT downloading begin.')
+	if history.url_never_downloaded( url ):
 		filename = 'tmp.%s.torrent' % (generator.hexdigest())
 		fetch_torrent(filename, url)
-		if torrent_never_downloaded(filename, conn) and os.system(cmd % (filename)) == 0:
+		if history.torrent_never_downloaded(filename) and os.system(cmd % (filename)) == 0:
 			print url, '->', filename
-			rt_log.log( 'Fetch %s .' % (url))
-			log_torrent(filename, url, conn)
+			rt_log.log( 'Fetch %s' % (url))
+			history.record(filename, url)
 			os.remove(filename)
+	rt_log.log('BT downloading end.')
 
-def bt_download_targets(cmd, rss_urls, regular_keywords, conn):
+def bt_download_targets(conf, history):
 	"""
 	Main loop : foreach all of rss urls and fetch target torrent file(s).
 	"""
 	global rt_log
-	for rss_url in rss_urls:
+	for rss_url in conf.rss_urls:
 		xmldoc = fetch_rss_xml(rss_url)
-		rt_log.log('Parse RSS : %s.' % (rss_url))
-		urls = get_target_torrent_urls( xmldoc, regular_keywords )
+		rt_log.log('Parse RSS : %s' % (rss_url))
+		urls = get_target_torrent_urls( xmldoc, conf.regular_keywords )
 		for url in urls:
-			bt_download_target(cmd, url, conn)
+			bt_download_target(conf.cmd, url, history)
+
+def check_running(pid_path):
+	try:
+		fd = open(pid_path, 'r')
+		print 'Instance %s is runing.' % (fd.read())
+		exit(127)
+	except IOError:
+		fd = open(pid_path, 'w')
+		fd.write(str(os.getpid()))
 
 def main():
 	global rt_log
-	(cmd, record_path, rss_urls, regular_keywords) = init_config('btdown.conf')
-	rt_log = btdown_log(record_path)
-	rt_log.log('BT downloading begin.')
-	conn = init_database('database')
+	conf = btdown_conf('btdown.conf')
+	check_running(conf.pid_path)
+	rt_log = btdown_log(conf.log_path)
+	history = torrent_history(conf.db_path)
 	rt_log.log('Database initialized.')
-	bt_download_targets(cmd, rss_urls, regular_keywords, conn)
-	rt_log.log('BT downloading end.')
-	conn.close()
+	bt_download_targets(conf, history)
+	os.remove(conf.pid_path)
 	return 0
-
 
 
 #
 # Real entry :
 #
 if __name__ == '__main__':
+	# Initialize encoding.
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
+	del sys.setdefaultencoding
+	# "main" routine.
 	exit(main())
