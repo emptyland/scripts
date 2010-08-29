19e8a42e57bf0fd2334a50068be6062a51df67c2
Niko Bellic <cjvoid@gmail.com>
2010-08-29 23:01:02 +0000

    modified:   ftool.py

--- ftool.py
+++ ftool.py
@@ -1,21 +1,42 @@
 #! /bin/env python
 
 
-import os, sys, pwd, grp
+import os, sys, pwd, grp, re
 
-def usage():
+def usage(comment = None):
+  exit_code = 0
+  if comment:
+    print comment
+    exit_code = 127
   print 'usage :'
   print '  ', sys.argv[0], 'show dir'
-  print '  ', sys.argv[0], 'chmod ddd fff dir'
+  print '  ', sys.argv[0], 'chmod dirmode filemode dir'
   print '  ', sys.argv[0], 'chown user group dir'
-  print '  ', sys.argv[0], 'delspace dir'
+  print '  ', sys.argv[0], 'rspace dir'
+  print '  ', sys.argv[0], 'genbkg static transition dir'
+  exit(exit_code)
 
+
+def fatal(comment):
+  print comment
+  exit(127)
+
+
+#
+# Walk routes :
+#
 def route_show(arg, dirname, names):
+  """Just show all files/directory
+  """
   print '> enter : ', os.path.realpath(dirname)
   for name in names:
     print '  ',name
 
+
 def route_chmod(arg, dirname, names):
+  """chmod files,
+  arg[0] : file's mode.
+  arg[1] : directory's mode."""
   print '> enter :', dirname
   for name in names:
     abs_path = os.path.realpath(dirname+'/'+name)
@@ -27,6 +48,9 @@ def route_chmod(arg, dirname, names):
   
 
 def route_chown(arg, dirname, names):
+  """change files/direcory's owner,
+  arg[0] : uid
+  arg[1] : gid"""
   print '> enter :', dirname
   for name in names:
     abs_path = os.path.realpath(dirname+'/'+name)
@@ -36,48 +60,93 @@ def route_chown(arg, dirname, names):
   print '< finally chown', dirname
   os.chown(os.path.realpath(dirname), arg[0], arg[1])
 
-def route_delspace(arg, dirname, names):
-  print 'delspace'
+
+def route_rspace(arg, dirname, names):
+  """replace filename's space to '_'.
+  """
+  print '> enter :', dirname
+  for name in names:
+    abs_path = os.path.realpath(dirname + '/' + name)
+    if not os.path.isdir(abs_path) and name.count(' ') > 0:
+      new = name.replace(' ', '_')
+      print '  mv', '"' + name + '"', '"' + new + '"'
+      # Must use the real(abs) path.
+      abs_new_path = os.path.realpath(dirname + '/' + new)
+      os.rename(abs_path, abs_new_path)
+  if dirname.count(' '):
+    new = dirname.replace(' ', '_')
+    print '< finally mv', dirname, new
+    abs_path = os.path.realpath(dirname)
+    abs_new_path = os.path.realpath(new)
+    os.rename(dirname, new)
+  else:
+    print '< leave', dirname
 
 
+def route_genbkg(arg, dirname, names):
+  """generate gnome backgroud goup's xml file.
+  arg[0] : static seconds, float number
+  arg[1] : transition seconds, float number
+  arg[2] : xml document object
+  """
+  pass
+
+#
+# Arguments Parsers:
+#
 def parse_none(argv):
+  """A none route dummy
+  """
   return ()
 
+
 def parse_mod_flags(argv):
+  pattern = '[0-7][0-7][0-7]'
+  if not re.match(pattern, argv[2]):
+    usage('Arguments error : ' + argv[2] + '.')
+  if not re.match(pattern, argv[3]):
+    usage('Arguments error : ' + argv[3] + '.')
   return (int(argv[2], 8), int(argv[3], 8))
 
+
 def parse_own_ids(argv):
-  uid = pwd.getpwnam(argv[2]).pw_uid
-  gid = grp.getgrnam(argv[3]).gr_gid
+  try:
+    uid = pwd.getpwnam(argv[2]).pw_uid
+    gid = grp.getgrnam(argv[3]).gr_gid
+  except KeyError:
+    fatal('Miss user/group name.')
   return (uid, gid)
 
 
+def parse_bkg_seconds(argv):
+  pattern = '\d{1,}\.\d{1,}'
+  if not re.match(pattern, argv[2]):
+    usage('Arguments error : ' + argv[2] + '.')
+  if not re.match(pattern, argv[3]):
+    usage('Arguments error : ' + argv[3] + '.')
+  return (float(argv[2]), float(argv[3]))
+
+
 if __name__ == '__main__':
-  # run_dir('bat-test')
+  # Actions profile mapping.
   action_map = {'show':[route_show, 3, parse_none],
                 'chmod':[route_chmod, 5, parse_mod_flags],
                 'chown':[route_chown, 5, parse_own_ids],
-                'delspace':[route_delspace, 3, parse_none]}
-
+                'rspace':[route_rspace, 3, parse_none],
+                'genbkg':[route_genbkg, 5, parse_bkg_seconds]}
+  # Check "argc" and "argc"
   try:
     action_profile = action_map[sys.argv[1]]
   except KeyError:
-    usage()
-    exit(0)
+    usage('Miss action ' + sys.argv[1] + '.')
   except IndexError:
-    usage()
-    exit(0)
-
-  #print 'action_map = ', action_map
-  #print 'match.argc = ', action_profile[1]
-  #print 'sys.argc = ', len(sys.argv)
+    usage('No action.')
   if len(sys.argv) != action_profile[1]:
-    usage()
-    exit(0)
-
+    usage('Miss arguments.')
   route = action_profile[0]
   parse = action_profile[2]
   path = os.path.realpath(sys.argv[action_profile[1] - 1])
-  #print 'route = ', route
-  #print 'path = ', path 
-  os.path.walk(path, route, parse(sys.argv))
+  try:
+    os.path.walk(path, route, parse(sys.argv))
+  except OSError as e:
+    fatal(str(e))
