adb1d9b796f1be570ff9de6730f52e5575a798b7
emptyland <cjvoid@gmail.com>
2014-04-08 16:44:05 +0800

    Update fish configure file.

--- conf/mac/config.fish
+++ conf/mac/config.fish
@@ -1,2 +1,14 @@
 alias pps='ps -ef | grep -v grep | grep --color=auto '
 alias ldd='otool -L '
+alias cm4='ssh wuke.cj@login1.cm4.taobao.org'
+alias cm3='ssh wuke.cj@login1.cm3.taobao.org'
+
+set -x JAVA_HOME (/usr/libexec/java_home)
+
+set kvm146 "wuke.cj@10.235.160.146"
+set kvm151 "wuke.cj@10.235.160.151"
+set kvm141 "wuke.cj@10.235.160.141"
+set kvm137 "wuke.cj@10.235.160.137"
+set kvm132 "wuke.cj@10.235.160.132"
+set kvm136 "wuke.cj@10.235.160.136"
+
